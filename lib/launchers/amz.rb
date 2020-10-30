257a7730255f762e83fcfb6795cd384f8559b6f0
Andrej Podhradsky <apodhrad@redhat.com>
2020-10-30 17:18:39 +0100

    Add aws_account_id required by OSD4
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- lib/launchers/amz.rb
+++ lib/launchers/amz.rb
@@ -47,6 +47,8 @@ module BushSlicer
         )
       }) )
       Aws.config.update( config[:config_opts].merge({region: region})) if region
+
+      @account_id = awscred["aws_account_id"]
     end
 
     private def client_ec2
@@ -521,6 +523,11 @@ module BushSlicer
     # @return [String]
     def secret_key
       ec2.client.config.credentials.secret_access_key
+    end
+
+    # @return [String]
+    def account_id
+      return @account_id
     end
 
     # @return [Object] undefined
