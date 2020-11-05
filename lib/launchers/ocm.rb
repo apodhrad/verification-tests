89d2ef0918b2c76f23e723228d6e63185608c249
Andrej Podhradsky <apodhrad@redhat.com>
2020-11-05 18:36:07 +0100

    Extend ocm.rb and add tests
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- lib/launchers/ocm.rb
+++ lib/launchers/ocm.rb
@@ -13,13 +13,16 @@ module BushSlicer
     include Common::Helper
 
     attr_reader :config
-    attr_reader :token, :token_file, :url, :region
+    attr_reader :token, :token_file, :url, :region, :version, :nodes, :lifespan, :cloud, :cloud_opts, :multi_az
 
     def initialize(**options) 
       service_name = options[:service_name] ||
                      ENV['OCM_SERVICE_NAME'] ||
                      'ocm'
-      @opts = default_opts(service_name).merge options
+      @opts = default_opts(service_name)&.merge options
+      unless @opts
+        @opts = options
+      end
 
       @token = ENV['OCM_TOKEN'] || @opts[:token]
       @token_file = @opts[:token_file]
@@ -31,8 +34,21 @@ module BushSlicer
           raise "You need to specify OCM token by 'token' or by 'token_file'"
         end
       end
+
       @url = ENV['OCM_URL'] || @opts[:url] || 'https://api.stage.openshift.com'
       @region = ENV['OCM_REGION'] || @opts[:region]
+      @version = ENV['OCM_VERSION'] || @opts[:version]
+      @nodes = ENV['OCM_NODES'] || @opts[:nodes]
+      @lifespan = ENV['OCM_LIFESPAN'] || @opts[:lifespan]
+      @multi_az = ENV['OCM_MULTI_AZ'] || @opts[:multi_az]
+
+      @cloud = ENV['OCM_CLOUD'] || @opts[:cloud]
+      if @cloud
+        @cloud_opts = default_opts(@cloud)
+        unless @cloud_opts
+          raise "Cannot find cloud '#{cloud}' defined in '#{service_name}'"
+        end
+      end
     end
 
     # @param service_name [String] the service name of this openstack instance
@@ -41,21 +57,54 @@ module BushSlicer
       return  conf[:services, service_name.to_sym]
     end
 
-    def generate_json_file
+    def generate_json(name)
       json_data = {
-        "name" => "myosd4",
-        "managed" => "true"
+        "name" => name,
+        "managed" => "true",
+        "multi_az" => "false",
+        "byoc" => "false"
       }
+
+      if @multi_az
+        json_data.merge!({"multi_az" => @multi_az})
+      end
+
       if @region
-        new_data = { "region" => { "id" => @region } }
-        json_data.merge!(new_data)
+        json_data.merge!({"region" => {"id" => @region}})
       end
-      json_file = Tempfile.new("ocm.json", Host.localhost.workdir)
-      puts json_file.path
-      File.open(json_file,"w") do |f|
-        f.write(json_data.to_json)
+
+      if @version
+        json_data.merge!({"version" => {"id" => "openshift-v#{@version}"}})
       end
-      puts json_file.read 
+
+      if @nodes
+        json_data.merge!({"nodes" => {"compute" => @nodes}})
+      end
+
+      if @lifespan
+        expiration = Time.now + 60 * 60 * @lifespan.to_i
+        json_data.merge!({"expiration_timestamp" => expiration.strftime("%Y-%m-%dT%H:%M:%SZ")})
+      end
+
+      if @cloud_opts
+        case @cloud_opts[:cloud_type]
+        when "aws"
+          aws = Amz_EC2.new(service_name: @cloud)
+          json_data.merge!({"aws" => {"access_key_id":aws.access_key, "secret_access":aws.secret_key, "account_id":aws.account_id}})
+          json_data.merge!({"byoc" => "true"})
+        end
+      end
+
+      return json_data.to_json
+    end
+
+    def create_osd(name)
+      url = "https://gitlab.cee.redhat.com/apodhrad/mk-performance-tests/-/raw/fix/scripts/osd-provision.sh?inline=false"
+      %x(
+        rm -rf /tmp/osd-provision.sh && \
+        curl #{url} --output /tmp/osd-provision.sh && \
+        chmod a+x /tmp/osd-provision.sh
+      )
     end
 
   end
