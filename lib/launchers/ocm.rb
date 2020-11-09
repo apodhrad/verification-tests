5ea44692bf1d416636f872958c286723da45dacd
Andrej Podhradsky <apodhrad@redhat.com>
2020-11-09 17:49:00 +0100

    Allow to specify a custom osd script
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- lib/launchers/ocm.rb
+++ lib/launchers/ocm.rb
@@ -7,6 +7,7 @@ end
 
 require 'common'
 require 'json'
+require 'tmpdir'
 
 module BushSlicer
   class OCM
@@ -16,9 +17,7 @@ module BushSlicer
     attr_reader :token, :token_file, :url, :region, :version, :nodes, :lifespan, :cloud, :cloud_opts, :multi_az
 
     def initialize(**options) 
-      service_name = options[:service_name] ||
-                     ENV['OCM_SERVICE_NAME'] ||
-                     'ocm'
+      service_name = ENV['OCM_SERVICE_NAME'] || options[:service_name] || 'ocm'
       @opts = default_opts(service_name)&.merge options
       unless @opts
         @opts = options
@@ -36,7 +35,7 @@ module BushSlicer
       end
 
       @url = ENV['OCM_URL'] || @opts[:url] || 'https://api.stage.openshift.com'
-      @region = ENV['OCM_REGION'] || @opts[:region]
+      @region = ENV['OCM_REGION'] || ENV['AWS_REGION'] || @opts[:region]
       @version = ENV['OCM_VERSION'] || @opts[:version]
       @nodes = ENV['OCM_NODES'] || @opts[:nodes]
       @lifespan = ENV['OCM_LIFESPAN'] || @opts[:lifespan]
@@ -99,29 +98,33 @@ module BushSlicer
     end
 
     def download_osd_script
-      script_url = "https://gitlab.cee.redhat.com/apodhrad/mk-performance-tests/-/raw/fix/scripts/osd-provision.sh?inline=false"
+      # we can consider putting the script directly into this repo
+      # we can also consider cloning the full git repo but downloading a single file is much faster
+      default_osd_script_url = "https://gitlab.cee.redhat.com/apodhrad/mk-performance-tests/-/raw/curl/scripts/osd-provision.sh?inline=false"
+      osd_script_url = ENV['OSD_SCRIPT_URL'] || default_osd_script_url
+      # we need another parent dir as the downloaded script downloads ocm tool to its parent
+      osd_scripts = "#{Dir.tmpdir}/osd-scripts"
       %x(
-        rm -rf /tmp/osd-scripts && \
-        mkdir /tmp/osd-scripts && \
-        curl -s #{script_url} --output /tmp/osd-scripts/osd-provision.sh && \
-        chmod a+x /tmp/osd-scripts/osd-provision.sh
+        rm -rf #{osd_scripts} && mkdir #{osd_scripts} && \
+        curl -s #{osd_script_url} --output #{osd_scripts}/osd-provision.sh && \
+        chmod a+x #{osd_scripts}/osd-provision.sh
       )
-      return "/tmp/osd-scripts/osd-provision.sh"
+      return "#{osd_scripts}/osd-provision.sh"
     end
 
     def create_osd(name)
+      # cerate a temp file with ocm-token
       ocm_token_file = Tempfile.new("ocm-token-file", Host.localhost.workdir)
       File.open(ocm_token_file, "w") do |f|
         f.write(@token)
       end
-      puts ocm_token_file.path
+      # create a temp file with cluster specification
       puts File.read(ocm_token_file.path)
       ocm_json_file = Tempfile.new("ocm-json-file", Host.localhost.workdir)
       File.open(ocm_json_file, "w") do |f|
         f.write(generate_json(name))
       end
-      puts ocm_json_file.path
-      puts File.read(ocm_json_file.path)
+      # now, download the script with we can create the OSD cluster
       osd_script = download_osd_script
       system("#{osd_script} --create --cloud-token-file #{ocm_token_file.path} -f #{ocm_json_file.path} --wait")
       system("#{osd_script} --get api_url -f #{ocm_json_file.path}")
--- lib/launchers/ocm_test.rb
+++ lib/launchers/ocm_test.rb
@@ -6,6 +6,12 @@ ENV['OCM_REGION'] = nil
 ENV['OCM_VERSION'] = nil
 ENV['OCM_LIFESPAN'] = nil
 
+lib_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+unless $LOAD_PATH.any? {|p| File.expand_path(p) == lib_path}
+  $LOAD_PATH.unshift(lib_path)
+end
+
+require 'common'
 require 'test/unit'
 require_relative './ocm'
 
@@ -54,5 +60,25 @@ class MyTest < Test::Unit::TestCase
     day = time.strftime("%d")
     assert_match(/.*"expiration_timestamp":"#{year}-#{month}-#{day}T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]Z".*/, json)
   end
+
+  def test_downloading_osd_script
+    options = { :token => "abc" }
+    ocm = BushSlicer::OCM.new(options)
+    osd_script = ocm.download_osd_script
+    assert(File.exists?("/tmp/osd-scripts/osd-provision.sh"), "File 'osd-provision.sh' was not downloaded")
+    content = File.read("/tmp/osd-scripts/osd-provision.sh")
+    assert_match(/.*ocm.*/, content)
+  end
+
+  #def test_downloading_osd_script_envvar
+  #  custom_osd_script = Tempfile.new("custom-osd-script.sh", Host.localhost.workdir)
+  #  custom_osd_script.write("ocm command --option value")
+  #  options = { :token => "abc" }
+  #  ocm = BushSlicer::OCM.new(options)
+  #  osd_script = ocm.download_osd_script
+  #  assert(File.exists?("/tmp/osd-scripts/osd-provision.sh"), "File 'osd-provision.sh' was not downloaded")
+  #  content = File.read("/tmp/osd-scripts/osd-provision.sh")
+  #  assert_match(/.*ocm.*/, content)
+  #end
 
 end
